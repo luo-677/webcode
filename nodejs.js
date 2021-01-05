03e48301b3ea1cca532d1987220469eff131fee3
luo-677 <70976038+luo-677@users.noreply.github.com>
2021-01-05 20:26:37 +0800

    1-5

--- nodejs.js
+++ nodejs.js
@@ -1,7 +1,7 @@
 const http = require('http');
 let port = 8888;
 let server = http.createServer((req, res) => {
-    res.writeHead({ 'content-type': 'text/html;charset=utf-8' });
+    res.writeHead(200, { 'content-type': 'text/html;charset=utf-8' });
     res.end('<h1>请求已收到</h1>');
 })
 server.listen(port, () => {
